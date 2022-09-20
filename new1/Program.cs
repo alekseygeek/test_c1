a46feccc072f68a08e2e11dd8ca97ef174f17903
Abashin Aleksey <mmilasomm@gmail.com>
2022-09-20 14:08:07 +0300

    программа какое число больше или меньше

--- new1/Program.cs
+++ new1/Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, ");
+﻿// показать 2 числа больше или меньше 
+Console.Clear();
+Console.Write("ведите первое число :");
+int a = int.Parse(Console.ReadLine());
+Console.Write("введите второе число :");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("наибольше  :" + a);
+    Console.WriteLine("наименьшее :" + b);
+}
+else
+{
+    Console.WriteLine("наибольше  :" + b);
+    Console.WriteLine("наименьшее :" + a);
+}
